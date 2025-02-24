2d69167f3dfca14540240df9259f6a7573c1b0c3
Cassiano26 <candidocassiano2@gmail.com>
2025-02-24 20:06:45 -0300

    feat: add basic api server

---
+++ src/server.js
@@ -0,0 +1,44 @@
+import http from 'node:http'
+
+const users = []
+
+const server = http.createServer(async (req, res) => {
+  const {method, url} = req
+
+  const buffers = []
+
+  for await (const chunk of req) {
+    buffers.push(chunk)
+  }
+
+  try {
+    req.body = JSON.parse(Buffer.concat(buffers).toString())
+  } catch {
+    req.body = null
+  }
+
+  console.log(req.body)
+
+  if(method === 'GET' && url === '/users') {
+    return res
+      .setHeader('content-type', 'application/json')
+      .end(JSON.stringify(users))
+  }
+
+  if(method === 'POST' && url === '/users') {
+    const {name, email} = req.body
+
+    users.push({
+      id: Math.random(),
+      name,
+      email,
+    })
+
+    return res.writeHead(201).end()
+  }
+
+  return res.writeHead(404).end('Not Found')
+})
+
+server.listen(3333)
+
