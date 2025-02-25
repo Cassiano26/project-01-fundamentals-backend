0ede1c65cc3a160a7411fe1d5870d74c81c1707b
Cassiano26 <candidocassiano2@gmail.com>
2025-02-25 12:53:18 -0300

    feat: add custom routes

--- src/server.js
+++ src/server.js
@@ -1,30 +1,19 @@
 import http from 'node:http'
-import { json } from './middlewares/json.js'
 
-const users = []
+import { json } from './middlewares/json.js'
+import { routes } from './middlewares/routes.js'
 
 const server = http.createServer(async (req, res) => {
   const {method, url} = req
 
   await json(req, res)
 
-  console.log(req.body)
+  const route = routes.find(route => {
+    return route.method === method && route.path === url
+  })
 
-  if(method === 'GET' && url === '/users') {
-    return res
-      .end(JSON.stringify(users))
-  }
-
-  if(method === 'POST' && url === '/users') {
-    const {name, email} = req.body
-
-    users.push({
-      id: Math.random(),
-      name,
-      email,
-    })
-
-    return res.writeHead(201).end()
+  if (route) {
+    return route.handler(req, res)
   }
 
   return res.writeHead(404).end('Not Found')
