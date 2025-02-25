c7814c467b8586c5183b4495cf926afcd2148583
Cassiano26 <candidocassiano2@gmail.com>
2025-02-25 13:59:17 -0300

    feat: add update route

--- src/middlewares/routes.js
+++ src/middlewares/routes.js
@@ -1,3 +1,4 @@
+import { buildRoutePath } from "../utils/build-route-path.js"
 import { Database } from "./database.js"
 import { randomUUID } from 'node:crypto'
 
@@ -6,7 +7,7 @@ const database = new Database()
 export const routes = [
   {
     method: 'GET',
-    path: '/users',
+    path: buildRoutePath('/users'),
     handler: (req, res) => {
       const users = database.select('users')
       return res.end(JSON.stringify(users))
@@ -14,7 +15,7 @@ export const routes = [
   },
   {
     method: 'POST',
-    path: '/users',
+    path: buildRoutePath('/users'),
     handler: (req, res) => {
       const {name, email} = req.body
 
@@ -28,6 +29,30 @@ export const routes = [
   
       return res.writeHead(201).end()
   
+    }
+  },
+  {
+    method: "PUT",
+    path: buildRoutePath("/users/:id"),
+    handler: (req, res) => {
+      const { id } = req.params
+
+      const { name, email } = req.body
+              
+      database.update('users', id, { name, email })
+
+      return res.writeHead(204).end()
+    }
+  },
+  {
+    method: "DELETE",
+    path: buildRoutePath("/users/:id"),
+    handler: (req, res) => {
+      const { id } = req.params
+
+      database.delete('users', id)
+
+      return res.writeHead(204).end()
     }
   }
 ]
